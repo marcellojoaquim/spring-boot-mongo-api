efa0b94fc104997d2b1ac822354c00b4f8fb6b20
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-06-06 16:16:51 -0300

    Start project

---
+++ src/test/java/com/springmongo/project/SpringmongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmongo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
