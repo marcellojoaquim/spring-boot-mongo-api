fee9146194666c066337aba426038b4fd9851fea
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-06-10 09:09:46 -0300

    fix dotenv configuration

--- src/main/java/com/springmongo/project/config/DatabaseConfig.java
+++ src/main/java/com/springmongo/project/config/DatabaseConfig.java
@@ -1,9 +1,9 @@
 package com.springmongo.project.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Configuration
+@ConfigurationProperties
 public class DatabaseConfig {
 
     private static final Dotenv dotenv = Dotenv.load();
@@ -11,7 +11,7 @@ public class DatabaseConfig {
     private static final String username = dotenv.get("DB_USERNAME");
     private static final String password = dotenv.get("DB_PASSWORD");
     private static final String host = dotenv.get("DB_HOST");
-    private static final int port = Integer.parseInt(dotenv.get("DB_PORT"));
+    private static final int port = (int)Integer.parseInt(dotenv.get("DB_PORT", "default value"));
     private static final String database = dotenv.get("DB_NAME");
 
 
