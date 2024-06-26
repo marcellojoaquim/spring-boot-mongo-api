7ef4120a12ba03207450c3c5730c35444f7321ac
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-06-25 22:00:25 -0300

    adde search by query methods

--- src/main/java/com/springmongo/project/resources/PostResources.java
+++ src/main/java/com/springmongo/project/resources/PostResources.java
@@ -1,6 +1,7 @@
 package com.springmongo.project.resources;
 
 import com.springmongo.project.domain.Post;
+import com.springmongo.project.resources.util.URL;
 import com.springmongo.project.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/posts")
@@ -20,5 +24,21 @@ public class PostResources {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post post = service.finById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")
+                                                      String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/titlesearchq")
+    public ResponseEntity<List<Post>> findByQueryTitle(@RequestParam(value = "text", defaultValue = "")
+                                                  String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/springmongo/project/services/PostService.java
+++ src/main/java/com/springmongo/project/services/PostService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,15 @@ public class PostService {
     public Post finById(String id){
         Optional<Post> post = repository.findById(id);
         return post.orElseThrow(()-> new ObjectNotFoundException("Object not found"));
+    }
+
+    @Transactional
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
+
+    @Transactional
+    public List<Post> findByTitleQuery(String text){
+        return repository.findByTitle(text);
     }
 }
